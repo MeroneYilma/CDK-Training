743c8873a44c9ce2441cf927c1d4cf73acf9dbb8
Yilma <meyilm@tcbna.net>
2024-10-26 23:22:32 -0500

    update

--- AWSControlTowerAccountFactory/account_creation_template/aws_control_tower_account_factory_stack/.env/Scripts/activate.bat
+++ AWSControlTowerAccountFactory/account_creation_template/aws_control_tower_account_factory_stack/.env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\AWSAccouontFactory\AWSControlTowerAccountFactory\AWSControlTowerAccountFactory\account_creation_template\.env
+set VIRTUAL_ENV=C:\GithubWF\CDK-Training\CDK-Training\AWSControlTowerAccountFactory\account_creation_template\aws_control_tower_account_factory_stack\.env
 
 if not defined PROMPT set PROMPT=$P$G
 
--- AWSControlTowerAccountFactory/account_creation_template/aws_sso_group_mapping_stack/.env/Scripts/activate.bat
+++ AWSControlTowerAccountFactory/account_creation_template/aws_sso_group_mapping_stack/.env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\AWSAccouontFactory\AWSControlTowerAccountFactory\account_creation_template\aws_sso_group_mapping_stack\.env
+set VIRTUAL_ENV=C:\GithubWF\CDK-Training\CDK-Training\AWSControlTowerAccountFactory\account_creation_template\aws_sso_group_mapping_stack\.env
 
 if not defined PROMPT set PROMPT=$P$G
 
